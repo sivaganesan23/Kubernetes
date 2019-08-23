003d93f45b0b4acc26d8cce27beb913ff28633f4
Sivakumar G <45412181+sivaganesan23@users.noreply.github.com>
2019-08-23 14:03:08 +0530

    Update modules.tf

--- eks-cluster/modules.tf
+++ eks-cluster/modules.tf
@@ -1,9 +1,7 @@
-// Create Route 53 hosted zone
-
 module "eks" {
   source                = "terraform-aws-modules/eks/aws"
   cluster_name          = "test-eks-cluster"
-  subnets               = ["subnet-dcf4e1b5", "subnet-904adcdd", "subnet-29d59f52"]
+  subnets               = ["subnet-81e9c7e9", "subnet-61cf002d", "subnet-8596f2ff"]
   tags                  = "${map("Environment", "test")}"
-  vpc_id                = "vpc-4271752b"
+  vpc_id                = "vpc-49101621"
 }
